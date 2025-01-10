467f14eb63245a529149839b86a2520606a8e27a
Matthew <matthewt.123719@gmail.com>
2025-01-10 11:27:10 -0800

    flush static folder

--- static/mqtt/dashboard.js
+++ static/mqtt/dashboard.js
@@ -1,57 +1,46 @@
-//SECTION 2: HANDLING FUNCTIONS
-function battery_update(battery_level) {
-    indicator = document.getElementById("battery_indicator")
-    indicator.style = `height:${battery_level}%`
-    // added color changes based on low battery
-    if (battery_level < 10) {
-        console.log(10)
-        indicator.setAttribute('class', "battery-level alert")
-    } else if (battery_level < 20) {
-        console.log(20)
-        indicator.setAttribute('class', "battery-level warn")
-    } 
-}
-//NEW: WebSocket
+
+/*************************
+ WEBSOCKET SETUP
+**************************/
 let chatSocket = 0;
 if (window.location.protocol == "https:") {
     chatSocket = new WebSocket(
         'wss://'
         + window.location.host
-        + '/ws/dashboard'
+        + '/ws/sensors'
     );
 } else {
     chatSocket = new WebSocket(
         'ws://'
         + window.location.host
-        + '/ws/dashboard'
+        + '/ws/sensors'
     );
 }
 
 
 chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-    switch (data['module']) {
-        case 'daq.speed':
-            //Speed Data
-            mph = data['content']
-            document.getElementById("center_gauge").innerHTML = `${Math.round(mph)} mph`
-            break;
-        case 'power_control.energy':
-            //Battery Data
-            battery_update(data['content'])
-            break;
-        //implement rest of the cases for all dashboard modules
-        default:
-            break;
-
+    console.log(`${data['module']}: ${data['content']}`)
+    switch(data['module'])
+    {
+        case 'humidity':
+            document.getElementById('humidity').innerHTML = data['content'];
+        case 'temperature':
+            document.getElementById('temperature').innerHTML = data['content'];
+        case 'light':
+            document.getElementById('light').innerHTML = data['content'];    
     }
 };
-
+chatSocket.onerror = function(evt) {
+    console.log(evt)
+}
 chatSocket.onclose = function(e) {
     console.error('Chat socket closed unexpectedly');
+    console.error(e)
+    self.close(e)
     const chatSocket = new WebSocket(
         'ws://'
         + window.location.host
-        + '/ws/dashboard'
+        + '/ws/sensors'
     );
 };
